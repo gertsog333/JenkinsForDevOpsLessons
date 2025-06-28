1579310181903bc228e1f7df720655e21744a8fb
gertsog333 <68449799+gertsog333@users.noreply.github.com>
2025-06-28 19:01:44 +0300

    Update webserverInstall.sh

--- webserverInstall.sh
+++ webserverInstall.sh
@@ -4,3 +4,5 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2
+sudo systemctl status apache2
+sleep 10
