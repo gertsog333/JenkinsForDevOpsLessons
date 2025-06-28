d63367639b66bc63853f108bb8f937c8a91f1ec6
gertsog333 <68449799+gertsog333@users.noreply.github.com>
2025-06-28 18:04:57 +0300

    Update webserverCheckLogs.sh

--- webserverCheckLogs.sh
+++ webserverCheckLogs.sh
@@ -3,15 +3,15 @@
 LOG_FILE="/var/log/apache2/access.log"
 
 if [ ! -f "$LOG_FILE" ]; then
-  echo "Ошибка: Лог-файл $LOG_FILE не найден"
+  echo "Error: Log file $LOG_FILE not found"
   exit 1
 fi
 
 ERRORS=$(grep -E '" (4[0-9][0-9]|5[0-9][0-9]) ' "$LOG_FILE")
 
 if [ -n "$ERRORS" ]; then
-  echo "Найдены ошибки 4xx и 5xx:"
+  echo "4xx and 5xx errors found::"
   echo "$ERRORS"
 else
-  echo "Ошибок нет"
+  echo "There are no errors in log file"
 fi
